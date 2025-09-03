e8aa7b827cd68aafadc24999928709d01419baf6
calliel.freitas <callieldefreitas@gmail.com>
2025-09-02 21:14:49 -0300

    mark: primeira aula completa

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+
+
+
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Button title={`Counter ${counter}`}
+              onPress={() => setCounter(counter + 1)}>
+       </Button>
       <StatusBar style="auto" />
     </View>
   );
